eae34787a0b34a785c4ce35848d44a58ba4f05b8
Timour Tricoire <81662309+tricoiret@users.noreply.github.com>
2024-12-03 16:37:06 -0500

    Day 3!

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -7,32 +7,32 @@ impl TryFrom<&str> for Report {
     type Error = ParseIntError;
 
     fn try_from(value: &str) -> Result<Self, Self::Error> {
-        value.split(' ')
-        .map(|i| {
-            i
-            .parse::<u32>()
-        })
-        .collect::<Result<Vec<u32>, _>>()
-        .map(|v| {Report(v)})
+        value
+            .split(' ')
+            .map(|i| i.parse::<u32>())
+            .collect::<Result<Vec<u32>, _>>()
+            .map(|v| Report(v))
     }
 }
 
 /// Returns list with the idxth element skipped
-fn skip<A>(list: &Vec<A>, idx: &usize) -> Vec<A> where A: Clone {
+fn skip<A>(list: &Vec<A>, idx: &usize) -> Vec<A>
+where
+    A: Clone,
+{
     // If this were a bigger project I'd define a custom iterator but
-    
-    list
-    .iter()
-    .enumerate()
-    .filter(|(i, _)| {i != idx})
-    .map(|(_, v)| v.clone())
-    .collect()
+
+    list.iter()
+        .enumerate()
+        .filter(|(i, _)| i != idx)
+        .map(|(_, v)| v.clone())
+        .collect()
 }
 
 impl Report {
     /// Reverses the inner list of the report
     fn reverse(&self) -> Self {
-        Self(self.0.iter().rev().map(|i|{i.clone()}).collect())
+        Self(self.0.iter().rev().map(|i| i.clone()).collect())
     }
 
     /// Checks whether the report is sorted
@@ -41,7 +41,7 @@ impl Report {
     }
 
     /**  
-     * Checks if, for an ascending report, every adjacent pair has an increase of at least 1 and at most 3 
+     * Checks if, for an ascending report, every adjacent pair has an increase of at least 1 and at most 3
      */
     fn proper_increases(&self) -> bool {
         self.0.windows(2).all(|pair| {
@@ -52,16 +52,14 @@ impl Report {
     }
 
     fn is_safe(&self) -> bool {
-        self.is_sorted() && self.proper_increases() 
-        || self.reverse().is_sorted() && self.reverse().proper_increases()
+        self.is_sorted() && self.proper_increases()
+            || self.reverse().is_sorted() && self.reverse().proper_increases()
     }
 
     fn test_removals(&self) -> Vec<Self> {
         (0..self.0.len())
-        .map(|idx| {
-            Self(skip(&self.0, &idx))
-        })
-        .collect()
+            .map(|idx| Self(skip(&self.0, &idx)))
+            .collect()
     }
 
     fn dampened_safe(&self) -> bool {
@@ -72,23 +70,27 @@ impl Report {
 fn main() -> Result<(), ParseIntError> {
     let inpt = include_str!("./input.txt");
 
-    let safe_reports = inpt.lines().filter(|&report_str | {
-        Report::try_from(report_str)
-        .expect(&format!("The line \"{report_str}\" is not a valid report"))
-        .is_safe()
-    })
-    .count();
+    let safe_reports = inpt
+        .lines()
+        .filter(|&report_str| {
+            Report::try_from(report_str)
+                .expect(&format!("The line \"{report_str}\" is not a valid report"))
+                .is_safe()
+        })
+        .count();
 
     println!("Part 1: {safe_reports} reports are safe");
 
     println!("Activating problem dampener...");
-    
-    let dampened_safe_reports = inpt.lines().filter(|&report_str | {
-        Report::try_from(report_str)
-        .expect(&format!("The line \"{report_str}\" is not a valid report"))
-        .dampened_safe()
-    })
-    .count();
+
+    let dampened_safe_reports = inpt
+        .lines()
+        .filter(|&report_str| {
+            Report::try_from(report_str)
+                .expect(&format!("The line \"{report_str}\" is not a valid report"))
+                .dampened_safe()
+        })
+        .count();
 
     println!("Part 2: {dampened_safe_reports} reports are safe");
 
